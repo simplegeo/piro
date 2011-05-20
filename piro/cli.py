e16d0ed3f3e9a89fe8efea3e9a86e6329d25fb78
Paul Lathrop <paul@simplegeo.com>
2011-05-19 18:34:54 -0700

    Clusto-ify cli.py

--- piro/cli.py
+++ piro/cli.py
@@ -1,19 +1,29 @@
 from argparse import ArgumentParser
 import sys
 
-from piro.services import Service
+import piro.clustohttp as clusto
+import piro.service as service
 
-ACTIONS=['start', 'stop', 'restart', 'status']
-SERVICES=['cassandra']
+CLUSTO = clusto.ClustoProxy('http://clusto.simplegeo.com/api')
+
+def get_contents(pool):
+    return set(CLUSTO.get_by_name(pool).contents())
+
+def get_hosts(pools):
+    return reduce(lambda p, i: p.intersection(i), map(get_contents, pools))
 
 def main():
     parser = ArgumentParser(description='Intelligently control services.')
-    parser.add_argument('action', choices=ACTIONS,
+    parser.add_argument('action', choices=service.ACTIONS,
                         help='The action you wish to apply to the service.')
-    parser.add_argument('service', choices=SERVICES,
+    parser.add_argument('service', choices=service.SERVICES,
                         help='Name of the service you wish to control.')
-    parser.add_argument('host', nargs='+',
-                        help='The host(s) which run the service you wish to control.')
+    parser.add_argument('pool', nargs='+', help='Set of clusto pools '
+                        'describing the hosts on which you wish to '
+                        'perform the service control action.')
     args = parser.parse_args()
-    service = Service.get_service(args.service, args)
-    return getattr(service, args.action)()
+    args.prod = 'production' in args.pool
+    args.service = args.service.lower().replace('-', '_')
+    hosts = get_hosts(args.pool)
+    return getattr(sys.modules['piro.service'],
+                   "%s_%s" % (args.action, args.service))(hosts, prod=args.prod)
