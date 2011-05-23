e82ded3df0dda01bdddf82cba8ac59de3767e077
Paul Lathrop <paul@simplegeo.com>
2011-05-23 15:27:45 -0700

    Add some command-line options.

--- piro/cli.py
+++ piro/cli.py
@@ -22,8 +22,13 @@ def main():
     parser.add_argument('pool', nargs='+', help='Set of clusto pools '
                         'describing the hosts on which you wish to '
                         'perform the service control action.')
+    parser.add_argument('-t', '--timeout', default=120, type=int,
+                        help='Timeout (for services/actions that support it.)')
+    parser.add_argument('--keyspace', default='system',
+                        help='Keyspace to check the cassandra ring state for, '
+                        'used by the cassandra and penelope actions.')
     args = parser.parse_args()
     args.prod = 'production' in args.pool
     hosts = get_hosts(args.pool)
     return getattr(sys.modules['piro.service'],
-                   args.action)(hosts, service=args.service, prod=args.prod)
+                   args.action)(hosts, service=args.service, args=args)
