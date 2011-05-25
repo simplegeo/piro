65ae7a0b72a9c8840eb2533811bc4b20442fee09
Paul Lathrop <paul@simplegeo.com>
2011-05-24 18:00:40 -0700

    Add docstrings.

--- piro/util/amazinghorse.py
+++ piro/util/amazinghorse.py
@@ -4,7 +4,6 @@ try:
     import simplejson as json
 except ImportError:
     import json
-from urllib import urlencode
 import urllib2 as url
 from piro.util import NoContentException
 
@@ -12,16 +11,16 @@ class AmazingHorse(object):
     """
     Client object for interacting with the amazinghorse API.
     """
-    def __init__(self, url='https://amazinghorse.simplegeo.com:4430/aws',
+    def __init__(self, base_url='https://amazinghorse.simplegeo.com:4430/aws',
                  username=None, password=None):
-        """
-        Initialize the amazinghorse object.
-        """
-        self.url = url
+        self.url = base_url
         auth = username + ':' + password
         self.auth = auth.encode('base64')
 
     def get_elbs(self):
+        """
+        Fetch the list of ELBs from the amazinghorse API
+        """
         req = url.Request(self.url + '/elb/')
         req.add_header('Authorization', 'Basic %s' % self.auth)
         res = url.urlopen(req)
@@ -31,6 +30,10 @@ class AmazingHorse(object):
         return json.loads(data)
 
     def get_elb(self, name):
+        """
+        Given an ELB name, fetch the info about that ELB from the
+        amazinghorse API.
+        """
         req = url.Request(self.url + '/elb/' + name)
         req.add_header('Authorization', 'Basic %s' % self.auth)
         res = url.urlopen(req)
@@ -40,9 +43,17 @@ class AmazingHorse(object):
         return json.loads(data)
 
     def get_availability_zones(self, elb_name):
+        """
+        Given an ELB name, fetch the active availability zones for
+        that ELB from the amazinghorse API.
+        """
         return self.get_elb(elb_name)['availability_zones']
 
     def enable_availability_zone(self, zone_name, elb_name):
+        """
+        Given the name of an availability zone and an ELB name,
+        activate that availability zone in that ELB.
+        """
         req = url.Request(self.url + '/elb/%s/%s?state=on' % (elb_name,
                                                               zone_name))
         req.add_header('Authorization', 'Basic %s' % self.auth)
@@ -53,6 +64,10 @@ class AmazingHorse(object):
         return json.loads(data)
 
     def disable_availability_zone(self, zone_name, elb_name):
+        """
+        Given the name of an availability zone and an ELB name,
+        deactivate that availability zone in that ELB.
+        """
         req = url.Request(self.url + '/elb/%s/%s?state=off' % (elb_name,
                                                                zone_name))
         req.add_header('Authorization', 'Basic %s' % self.auth)
